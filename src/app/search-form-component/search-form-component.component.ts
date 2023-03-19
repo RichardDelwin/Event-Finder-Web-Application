9f0ce385b24cc9ab025338668605edc428c71ac0
RichardDelwin <35023976+RichardDelwin@users.noreply.github.com>
2023-03-19 02:40:50 -0700

    Added search form component

---
+++ src/app/search-form-component/search-form-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-form-component',
+  templateUrl: './search-form-component.component.html',
+  styleUrls: ['./search-form-component.component.css']
+})
+export class SearchFormComponentComponent {
+
+}
