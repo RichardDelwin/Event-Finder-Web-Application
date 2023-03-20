341e21cb076b1753ee48f60f283e53ca631ce3d0
RichardDelwin <35023976+RichardDelwin@users.noreply.github.com>
2023-03-19 23:58:55 -0700

    favorites component

---
+++ src/app/favorites-form/favorites-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favorites-form',
+  templateUrl: './favorites-form.component.html',
+  styleUrls: ['./favorites-form.component.css']
+})
+export class FavoritesFormComponent {
+
+}
