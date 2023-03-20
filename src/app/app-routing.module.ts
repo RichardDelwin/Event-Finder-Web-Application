e981342ab83ff6b88eb6456bfc453f47ce20a2d4
RichardDelwin <35023976+RichardDelwin@users.noreply.github.com>
2023-03-19 23:56:02 -0700

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SearchFormComponentComponent} from "./search-form-component/search-form-component.component";
+import {FavoritesFormComponent} from "./favorites-form/favorites-form.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "search", component:SearchFormComponentComponent},
+  {path: "favorites", component:FavoritesFormComponent},
+  {path: "", redirectTo:"/search", pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
