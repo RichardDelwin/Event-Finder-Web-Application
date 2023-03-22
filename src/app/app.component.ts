1fb4405d08766ab72dc9ee3443bf93668d968676
RichardDelwin <35023976+RichardDelwin@users.noreply.github.com>
2023-03-22 12:20:31 -0700

    Added autocomplete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ComponentUpdateService} from "./component-update.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HW8v2';
+  showResultsTable : boolean = false;
+  constructor(private componentUpdateService : ComponentUpdateService) {
+    componentUpdateService.showResultsTable.subscribe(value=>this.showResultsTable = value);
+  }
 }
