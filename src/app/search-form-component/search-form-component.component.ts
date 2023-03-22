e4e2abb54761e306e3ece1f8acbed06d76782e05
RichardDelwin <35023976+RichardDelwin@users.noreply.github.com>
2023-03-22 12:22:58 -0700

    Added autocomplete

--- src/app/search-form-component/search-form-component.component.ts
+++ src/app/search-form-component/search-form-component.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import {NgForm} from "@angular/forms";
-import {DataServiceService} from "./data-service.service";
+import {Component, OnInit} from '@angular/core';
+import {FormControl, NgForm} from "@angular/forms";
+import {DataServiceService} from "../data-service.service";
+import {ComponentUpdateService} from "../component-update.service";
+import { debounceTime, tap, distinctUntilChanged, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search-form-component',
@@ -8,29 +10,73 @@ import {DataServiceService} from "./data-service.service";
   styleUrls: ['./search-form-component.component.css']
 })
 
-export class SearchFormComponentComponent {
+export class SearchFormComponentComponent implements OnInit{
   isChecked: any;
   addressIsDisabled: boolean = false;
   selectedCategory: string = 'Default';
   selectedLocation: string = '';
   enteredKeyword: string = '';
   enteredDistance: string = '';
+  searchMoviesCtrl = new FormControl();
+  minLengthTerm = 1;
+  filteredMovies: string[] = [];
+  isLoading = false;
+  constructor(private dataService : DataServiceService, private componentUpdateService: ComponentUpdateService) {
+  }
 
-  constructor(private dataService : DataServiceService) {
+  displayWith(value: any) {
+    return value;
+  }
+  ngOnInit(){
+    this.searchMoviesCtrl.valueChanges
+      .pipe(
+        filter(res => {
+          if(res.length == 0){
+            this.filteredMovies=[];
+          }
+          return res !== null && res.length >= this.minLengthTerm;
+        }),
+        distinctUntilChanged(),
+        debounceTime(1000),
+      tap(()=>{
+        this.filteredMovies=[];
+        this.isLoading= true})).
+    subscribe(keyword=>{
+      this.autoComplete(keyword);
+      console.log(this.filteredMovies);
+    })
+  }
+
+
+  autoComplete(keyword : string){
+
+    const url = "http://localhost:3000/autocomplete?keyword="+keyword;
+    fetch(url)
+      .then(res=> res.json())
+      .then(res=>{
+        console.log(res);
+        this.filteredMovies=[]
+
+        for(let i=0; i<res.length; i++){
+          this.filteredMovies.push(res[i]);
+        }
+        this.isLoading=false;
+      })
+    console.log("filteredMovies = ",this.filteredMovies);
   }
 
   async onSearch(f: NgForm) {
 
+    this.filteredMovies=[];
     console.log(f);
     let formData = {
-      keyword: f.value["keyword-field"],
+      keyword: this.enteredKeyword,
       category: f.value["category-field"],
       distance: (f.value["distance-field"] =="")? 10: f.value["distance-field"],
       latitude:"",
       longitude: ""
     }
 
-    console.log(formData);
     if(this.isChecked){
       let location = await this.dataService.getCurrentLocation()
       formData.latitude = location.latitude;
@@ -43,8 +89,9 @@ export class SearchFormComponentComponent {
         formData.longitude = location.longitude;
       }
     }
-
     console.log(formData);
+    this.dataService.getEventRecords(formData);
+    this.componentUpdateService.emitChange(true);
   }
 
   onClear() {
@@ -54,6 +101,7 @@ export class SearchFormComponentComponent {
     this.selectedLocation = '';
     this.addressIsDisabled = false;
     this.isChecked = false;
+    this.componentUpdateService.emitChange(false);
   }
 
   checkboxUpdated() {
@@ -64,5 +112,9 @@ export class SearchFormComponentComponent {
     else{
       this.addressIsDisabled = false;
     }
+  }
+
+  onSelected() {
+    this.enteredKeyword = this.enteredDistance;
   }
 }
