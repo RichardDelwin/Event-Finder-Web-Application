3a71486b6424e7c116fc447fe4a7a1c8bac46d30
RichardDelwin <35023976+RichardDelwin@users.noreply.github.com>
2023-03-23 01:14:24 -0700

    component update service for toggling component visbility

--- src/app/component-update.service.ts
+++ src/app/component-update.service.ts
@@ -10,9 +10,24 @@ export class ComponentUpdateService {
   // private results_TableRecords = new Subject<any>();
   showResultsTable = this.show_ResultsTable.asObservable();
 
+  private showEventsCardSubject = new Subject<boolean>();
+  showEventCard = this.showEventsCardSubject.asObservable();
+
+  private tableVisibilityStatusSubject = new Subject<string>();
+  tableVisibilityStatus = this.tableVisibilityStatusSubject.asObservable();
+
+
   constructor() { }
 
   emitChange(change: any){
     this.show_ResultsTable.next(change);
+  }
+
+  updateTableVisibilityOnly(change: string){
+    this.tableVisibilityStatusSubject.next(change);
+  }
+
+  emitEventCardStatus(change: boolean){
+    this.showEventsCardSubject.next(change);
   }
 }
