3594c956bb14186256034dd1cc580d213d6027f8
RichardDelwin <35023976+RichardDelwin@users.noreply.github.com>
2023-03-23 01:22:33 -0700

    Added events details card and implemented  events tab.

---
+++ src/app/event-tab/event-tab.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import {DataServiceService} from "../data-service.service";
+import {ComponentUpdateService} from "../component-update.service";
+import {eventDetailsType} from "../types/eventDetails";
+
+@Component({
+  selector: 'app-event-tab',
+  templateUrl: './event-tab.component.html',
+  styleUrls: ['./event-tab.component.css']
+})
+export class EventTabComponent {
+
+  eventDetails: eventDetailsType | undefined;
+  constructor(private dataService : DataServiceService, private componentUpdateService : ComponentUpdateService) {
+    dataService.eventDetails.subscribe(value=>this.eventDetails = value);
+  }
+
+  backClicked() {
+    console.log("Clicked");
+    this.componentUpdateService.updateTableVisibilityOnly("visible");
+    this.componentUpdateService.emitEventCardStatus(false);
+    // this.componentUpdateService.emitChange(true);
+    console.log(this.eventDetails);
+  }
+}
