51ee04339b34965c8b03625bf6e9c7a941967118
RichardDelwin <35023976+RichardDelwin@users.noreply.github.com>
2023-03-23 01:15:45 -0700

    Event details type

---
+++ src/app/types/eventDetails.ts
@@ -0,0 +1,14 @@
+export interface eventDetailsType {
+  name: string,
+  attractions: string[],
+  attractionsUrl: string[],
+  classifications: string[],
+  priceRanges_min: Number,
+  priceRanges_max: Number,
+  ticketStatus: string,
+  buyAt: string,
+  seatMap: string,
+  localDate: string,
+  localTime: string,
+  venue: string
+}
