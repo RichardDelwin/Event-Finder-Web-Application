d4e36f9cfb350f40e43758ccf3a374ffb9c687a8
RichardDelwin <35023976+RichardDelwin@users.noreply.github.com>
2023-03-27 00:03:38 -0700

    Added Gmaps Modal.

--- src/app/gmaps-model/gmaps-model.component.ts
+++ src/app/gmaps-model/gmaps-model.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 import { GoogleMapsModule } from '@angular/google-maps'
+import {DataServiceService} from "../data-service.service";
 @Component({
   selector: 'app-gmaps-model',
   templateUrl: './gmaps-model.component.html',
   styleUrls: ['./gmaps-model.component.css']
 })
 export class GmapsModelComponent {
+
+  markerPosition : any | undefined;
+  mapOptions: google.maps.MapOptions | undefined;
+  constructor(private dataService : DataServiceService) {
+    dataService.venueDetails.subscribe(value=> this.updateData(value));
+  }
+
+  updateData(value: any){
+
+    this.markerPosition = {lat: value.venueLocation.latitude, lng: value.venueLocation.longitude};
+
+    this.mapOptions = {
+      center: this.markerPosition || null,//{ lat: 38.9987208, lng: -77.2538699 },
+      zoom : 14
+    }
+
+    console.log("UpdateData", value);
+  }
+
 
 }
