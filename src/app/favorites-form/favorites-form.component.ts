38a93de51ce5cd962d49d301e74f8fabc280b593
RichardDelwin <35023976+RichardDelwin@users.noreply.github.com>
2023-03-27 00:03:05 -0700

    Added favorites component.

--- src/app/favorites-form/favorites-form.component.ts
+++ src/app/favorites-form/favorites-form.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-favorites-form',
   templateUrl: './favorites-form.component.html',
   styleUrls: ['./favorites-form.component.css']
 })
-export class FavoritesFormComponent {
+export class FavoritesFormComponent implements OnInit{
+  allRecordsInFavs: any;
 
+  ngOnInit(): void {
+
+    console.log("Favs table");
+    this.updateRecords();
+  }
+
+  updateRecords(){
+    let tableValues = localStorage.getItem("fav-table");
+
+    if(tableValues!==null) {
+      this.allRecordsInFavs = JSON.parse(tableValues);
+    }else{
+      this.allRecordsInFavs = null;
+    }
+  }
+
+  deleteItem(id: any) {
+    alert("Removed from Favorites!");
+    localStorage.removeItem(id);
+
+    let jsonData : any[] = [];
+
+    if("fav-table" in localStorage){
+      jsonData = JSON.parse(localStorage.getItem("fav-table") || '');
+    }
+
+    jsonData = jsonData.filter(item=> item.id != id)
+
+    let finalData = JSON.stringify(jsonData);
+
+    localStorage.setItem("fav-table", finalData);
+
+    this.updateRecords();
+  }
 }
